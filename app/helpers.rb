05ca1290175f0a68298f03a575fcd85b1f3d521e
Hendrik Mans <hendrik@mans.de>
2012-05-26 13:06:26 +0200

    Use actual Twitter name

--- app/helpers.rb
+++ app/helpers.rb
@@ -18,7 +18,7 @@ class OmniAuthCallback < Freddie::Application
           auth = request.env['omniauth.auth']
 
           user = User.find_or_initialize_by :identity => [auth['provider'], auth['uid']]
-          user.name = "Anon"
+          user.name = auth['info']['nickname'] || auth['info']['name']
           user.save!
 
           session['user_id'] = user.id
