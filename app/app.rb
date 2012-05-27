958ae9a918061a4a48109963b2ea7dc93d0160e4
Hendrik Mans <hendrik@mans.de>
2012-05-27 15:18:15 +0200

    Only load current user once per request :)

--- app/app.rb
+++ app/app.rb
@@ -15,7 +15,7 @@ module HamburgIo
       end
 
       def current_user
-        if session['user_id']
+        @current_user ||= if session['user_id']
           User.find(session['user_id'])
         end
       end
